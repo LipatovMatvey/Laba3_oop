5e3ed644dc174c28428a309c37557e5195f04e52
Nikita <nikita_3307@mail.ru>
2026-04-02 19:30:10 +0300

    Исправлена ошибка с удалением объектов.

--- Laba3_oop/Form1.cs
+++ Laba3_oop/Form1.cs
@@ -200,7 +200,7 @@ namespace Laba3_oop
                     }
                     UpdateObjectsCount();
                     UpdateObjectsList();
-                    if (CurrentShop.Name == deletedName) resetFields();
+                    resetFields();
                     txtDisplayInfo.Text = "";
                     BoxMessage.ShowNativeMessageBox("Успех", "Объект удален", BoxMessage.SuccessCode);
                 }
